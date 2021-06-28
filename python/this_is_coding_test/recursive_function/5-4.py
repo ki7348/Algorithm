814d750df808886441a94b75fbe1130a00f5be8d
DongKi Lee <ki7348@naver.com>
2021-06-28 15:31:24 +0900

    first commit

---
+++ python/this_is_coding_test/recursive_function/5-4.py
@@ -0,0 +1,12 @@
+#재귀 함수 종료 예제
+
+import sys
+
+def recursive_function(i):
+    if i == 100:
+        return
+    print(i, '번째 재귀 함수에서', i+1, '번째 재귀함수를 호출합니다.' )
+    recursive_function(i+1)
+    print(i, '번재 재귀 함수를 종료합니다.')
+
+recursive_function(1)
