45e138c282cef3f60c703c5c8dfa7703b93622d2
DongKi Lee <ki7348@naver.com>
2021-07-16 21:12:40 +0900

    first commit

---
+++ python/boj/MST/9372.py
@@ -0,0 +1,38 @@
+def find_parent(parent, x):
+    if parent[x]!=x:
+        parent[x]=find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+case=int(input())
+for i in range(case):
+    v, e = map(int, input().split())
+    parent = [0]*(v+1)
+
+    edges=[]
+    result=0
+
+    for i in range(1, v+1):
+        parent[i]=i
+
+    for _ in range(e):
+        a, b=map(int, input().split())
+        edges.append((a, b))
+
+    edges.sort()
+
+    for edge in edges:
+        a, b =edge
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            result+=1
+            
+
+    print(result)
